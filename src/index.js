b4450c309c9225ce5ad6d2490da0fe0d317aa14b
Jack Power <2012jackpower@gmail.com>
2021-11-03 21:43:28 +0000

    Added Upcoming movies page.

--- src/index.js
+++ src/index.js
@@ -5,13 +5,15 @@ import HomePage from "./pages/homePage";
 import MoviePage from "./pages/movieDetailsPage";
 import FavoriteMoviesPage from "./pages/favoriteMoviesPage"; 
 import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from './components/siteHeader'
+import SiteHeader from './components/siteHeader';
+import UpcomingMoviesPage from "./pages/upcomingMoviesPage";
 
 const App = () => {
   return (
       <BrowserRouter>
           <SiteHeader />      {/* New Header  */}
           <Switch>
+          <Route exact path="/movies/upcoming" component={UpcomingMoviesPage} />
          <Route path="/reviews/:id" component={MovieReviewPage} />
         <Route exact path="/movies/favorites" component={FavoriteMoviesPage} />
         <Route path="/movies/:id" component={MoviePage} />
