1095dbf49aebb5ae2883c36606af77ced246eeaf
Jack Power <2012jackpower@gmail.com>
2021-11-08 12:45:42 +0000

    Bug Fix - make action icons on movie card configurable.

--- src/components/movieCard/index.js
+++ src/components/movieCard/index.js
@@ -25,7 +25,7 @@ const useStyles = makeStyles({
   },
 });
 
-export default function MovieCard({ movie }) {
+export default function MovieCard({ movie, action }) {
   const classes = useStyles();
   const { favorites, addToFavorites } = useContext(MoviesContext);
 
@@ -81,9 +81,10 @@ export default function MovieCard({ movie }) {
         </Grid>
       </CardContent>
       <CardActions disableSpacing>
-      <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
+      {action(movie)}
+      {/* <IconButton aria-label="add to favorites" onClick={handleAddToFavorite}>
         <FavoriteIcon color="primary" fontSize="large" />
-    </IconButton>
+    </IconButton> */}
         <Link to={`/movies/${movie.id}`}>
         <Button variant="outlined" size="medium" color="primary">
           More Info ...
--- src/pages/favoriteMoviesPage.js
+++ src/pages/favoriteMoviesPage.js
@@ -3,7 +3,9 @@ import PageTemplate from "../components/templateMovieListPage";
 import { MoviesContext } from "../contexts/moviesContext";
 import { useQueries } from "react-query";
 import { getMovie } from "../api/tmdb-api";
-import Spinner from '../components/spinner'
+import Spinner from '../components/spinner';
+import RemoveFromFavorites from "../components/cardIcons/removedFromFavorites";
+import WriteReview from "../components/cardIcons/writeReview";
 
 const FavoriteMoviesPage = () => {
   const {favorites: movieIds } = useContext(MoviesContext);
@@ -28,10 +30,17 @@ const FavoriteMoviesPage = () => {
 
   return (
     <PageTemplate
-      title="Favourite Movies"
-      movies={movies}
-      selectFavorite={toDo}
-    />
+    title="Favorite Movies"
+    movies={movies}
+    action={(movie) => {
+      return (
+        <>
+          <RemoveFromFavorites movie={movie} />
+          <WriteReview movie={movie} />
+        </>
+      );
+    }}
+  />
   );
 };
 
